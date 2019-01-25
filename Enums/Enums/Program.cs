9418f289ddc457159f62c87acc058f339ecc9daf
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-01-25 09:49:56 +0600

    2

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int result = (int)StatusCodes.Ok;
+            //Console.WriteLine(DayOfWeek.Monday);
+
+            string dayOfWeek = "Friday";
+            DayOfWeek dayOfWeekEnums;
+            if(Enum.TryParse(dayOfWeek, out dayOfWeekEnums))
+            {
+                //logika raboty c enum
+            }
+
+            Point point = new Point(1, 2);
+            ChangePoint(point);
+            Console.WriteLine($"{point.X}, {point.Y}");
+
+            Shop shop = new Shop
+            {
+                Name = null,//"Flip.kz",
+                Address = "Караганда, ул. Какая-то",
+                MapPoint = null//new Point(45, 12)
+            };
+            Console.WriteLine(shop.MapPoint.Value.X);
+
+
+            Console.ReadLine();
+        }
+
+        public static void ChangePoint(Point point)
+        {
+            point.X = 3;
+            point.Y = 5;
+        }
+    }
+}
